16453df83d719ee89c1fc428e651b850ea3de17e
Mike Aizatsky <maizatskyi@cloudflare.com>
2024-07-25 09:31:49 -0700

    first version

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # gen-compile-commands
+
+Utility to generate `compile-commands.json` based on `compile-flags.txt`
+
+```
+Usage: gen-compile-commands [OPTIONS] --root <ROOT> --compile-flags <COMPILE_FLAGS> --out <OUT>
+
+Options:
+      --root <ROOT>                    Root project directory, all files in `compile_commands.json` will be specified relative to this path
+      --compile-flags <COMPILE_FLAGS>  Path to the `compile_flags.txt`. Used for every file in `compile_commands.json`
+      --out <OUT>                      Path to the resulting `compile_commands.json`
+      --src-dir <SRC_DIR>              Directory to scan for source files
+      --ext <EXT>                      Source file extensions to include in `compile_commands.json` [default: c++ cc]
+  -h, --help                           Print help
+```
---
+++ src/main.rs
@@ -0,0 +1,98 @@
+//! Simple `compile_commands.json` generator.
+
+use clap::Parser;
+use serde::Serialize;
+use std::{
+    fs::{self},
+    path::PathBuf,
+};
+use walkdir::WalkDir;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Root project directory, all files in `compile_commands.json` will be specified relative
+    /// to this path.
+    #[arg(long)]
+    root: PathBuf,
+
+    /// Path to the `compile_flags.txt`. Used for every file in `compile_commands.json`.
+    #[arg(long)]
+    compile_flags: PathBuf,
+
+    /// Path to the resulting `compile_commands.json`.
+    #[arg(long)]
+    out: PathBuf,
+
+    /// Directory to scan for source files.
+    #[arg(long)]
+    src_dir: Vec<PathBuf>,
+
+    /// Source file extensions to include in `compile_commands.json`.
+    #[arg(long, default_values_t=["c++".to_string(), "cc".to_string()])]
+    ext: Vec<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let flags = fs::read_to_string(&args.compile_flags).unwrap_or_else(|err| {
+        panic!(
+            "error reading compile flags from {}: {err}",
+            args.compile_flags.display()
+        )
+    });
+
+    let flags: Vec<String> = flags
+        .split('\n')
+        .filter_map(|l| {
+            if l.is_empty() {
+                None
+            } else {
+                Some(l.to_owned())
+            }
+        })
+        .collect();
+
+    let mut entries = vec![];
+    for dir in args.src_dir {
+        for entry in WalkDir::new(dir).into_iter().filter_entry(|e| {
+            e.file_type().is_dir()
+                || args.ext.contains(
+                    &e.path()
+                        .extension()
+                        .unwrap_or_default()
+                        .to_string_lossy()
+                        .to_string(),
+                )
+        }) {
+            let entry = entry.unwrap();
+            if entry.file_type().is_dir() {
+                continue;
+            }
+
+            entries.push(CompilationDatabaseEntry {
+                directory: args.root.to_string_lossy().to_string(),
+                file: entry
+                    .path()
+                    .strip_prefix(&args.root)
+                    .unwrap()
+                    .to_string_lossy()
+                    .to_string(),
+                arguments: flags.clone(),
+                output: None,
+            });
+        }
+    }
+
+    fs::write(args.out, serde_json::to_string_pretty(&entries).unwrap()).unwrap();
+}
+
+/// <https://clang.llvm.org/docs/JSONCompilationDatabase.html/>
+#[derive(Serialize)]
+struct CompilationDatabaseEntry {
+    directory: String,
+    arguments: Vec<String>,
+    file: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    output: Option<String>,
+}
